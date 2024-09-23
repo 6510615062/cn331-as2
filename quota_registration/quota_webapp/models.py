dc5d0b2194206a272eed8067b023aaac681a2e18
Tanapat Sa-nguantud <6510615120@cn331>
2024-09-23 22:23:22 +0700

    Add str function in models

--- quota_registration/quota_webapp/models.py
+++ quota_registration/quota_webapp/models.py
@@ -4,6 +4,9 @@ class Student(models.Model):
     student_ID = models.CharField(max_length=20)
     student_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.student_ID + ":" + self.student_name
+
 class Course(models.Model):
     course_ID = models.CharField(max_length=20)
     course_name = models.CharField(max_length=50)
@@ -13,6 +16,12 @@ class Course(models.Model):
     current_registration = models.IntegerField(default=0)
     status = models.BooleanField(default=1)
 
+    def __str__(self):
+        return self.course_ID
+
 class Registration(models.Model):
     student_ID = models.ForeignKey(Student, on_delete=models.CASCADE)
     course_ID = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.student_ID) + ":" + str(self.course_ID)
