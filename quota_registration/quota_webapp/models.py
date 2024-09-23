da91920208526af3d2adfa0b6abde13507933b41
Tanapat Sa-nguantud <6510615120@cn331>
2024-09-23 15:52:31 +0700

    Complete models.py

--- quota_registration/quota_webapp/models.py
+++ quota_registration/quota_webapp/models.py
@@ -7,16 +7,11 @@ class Student(models.Model):
 class Course(models.Model):
     course_ID = models.CharField(max_length=20)
     course_name = models.CharField(max_length=50)
+    semester = models.IntegerField(default=0)
+    academic_year = models.IntegerField(default=0)
     max_capacity = models.IntegerField(default=0)
     current_registration = models.IntegerField(default=0)
-
-class Professor(models.Model):
-    prof_ID = models.CharField(max_length=20)
-    prof_name = models.CharField(max_length=50)
-
-class Teaches(models.Model):
-    prof_ID = models.ForeignKey(Professor, on_delete=models.CASCADE)
-    course_ID = models.ForeignKey(Course, on_delete=models.CASCADE)
+    status = models.BooleanField(default=1)
 
 class Registration(models.Model):
     student_ID = models.ForeignKey(Student, on_delete=models.CASCADE)
