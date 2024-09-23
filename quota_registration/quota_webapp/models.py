a6f9ecebbd2e008b6439240359c93edec109a5e9
6510615062 <110813901+6510615062@users.noreply.github.com>
2024-09-23 05:09:47 +0000

    creat prototype classes

--- quota_registration/quota_webapp/models.py
+++ quota_registration/quota_webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    student_ID = models.CharField(max_length=20)
+    student_name = models.CharField(max_length=50)
+
+class Course(models.Model):
+    course_ID = models.CharField()
+    course_name = models.CharField()
+    max_capacity = models.IntegerField(default=0)
+    current_registration = models.IntegerField(default=0)
+
+class Professor(models.Model):
+    prof_ID = models.CharField()
+    prof_name = models.CharField()
+
+class Teaches(models.Model):
+    prof_ID = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    course_ID = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Registration(models.Model):
+    student_ID = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_ID = models.ForeignKey(Course, on_delete=models.CASCADE)
