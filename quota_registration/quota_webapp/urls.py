ded2fc5db793c3810943e4c6bbdd99baaca82686
Tanapat Sa-nguantud <6510615120@cn331>
2024-10-04 21:55:34 +0700

    Add register page

--- quota_registration/quota_webapp/urls.py
+++ quota_registration/quota_webapp/urls.py
@@ -4,6 +4,7 @@ from quota_webapp import views
 
 urlpatterns = [
     path("", views.index),
+    path("sign-up", views.sign_up, name="sign-up"),
     path("sign-in", views.sign_in, name="sign-in"),
     path("sign-out", views.sign_out),
     path("dashboard", views.dashboard, name="dashboard"),
